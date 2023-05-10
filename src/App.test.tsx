0f05abe71b4378496d14494af98f6dd9ff5ff13b
Rustam <isaev.rustam.a@gmail.com>
2023-05-10 18:09:04 +0300

    vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,8 @@ import App from './App';
 describe('', () => {
   it('should <App/> component', () => {
     render(<App />);
-    screen.debug();
-    expect(screen.queryByRole('button', { name: /btn/i })).toBeInTheDocument();
+    // screen.debug();
+    // expect(screen.queryByRole('button', { name: /btn/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /btn/i })).toBeInTheDocument();
   });
 });
